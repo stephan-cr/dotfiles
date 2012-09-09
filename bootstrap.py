9a131c850a3ea3cca3c92d1e86de3393f262df7c
stephan <stephan.cr@gmx.de>
2012-09-09 21:46:47 +0200

    fixed bootstrap

--- bootstrap.py
+++ bootstrap.py
@@ -16,6 +16,7 @@ with open('config.json', 'r') as f:
 for source, link_name in links.iteritems():
     source_path = os.path.join(CWD, source)
     link_path = os.path.join(HOME, link_name)
-    if os.access(link_path, os.F_OK) and \
-            not (os.readlink(link_path) == source_path):
+    file_exists = os.access(link_path, os.F_OK)
+    if not file_exists or (file_exists and \
+            not (os.readlink(link_path) == source_path)):
         os.symlink(source_path, link_path)
