a8adc3e4cb4cc8cca30703fbaf4309c7829314c8
stephan <stephan.cr@gmx.de>
2013-05-13 23:07:06 +0200

    rename file and its buffer

--- init.el
+++ init.el
@@ -578,3 +578,17 @@ The function assumes that the user set the variables `user-full-name' and
     (eval-print-last-sexp)))
 
 (el-get 'sync)
+
+;; http://emacsredux.com/blog/2013/05/04/rename-file-and-buffer/
+(defun rename-file-and-buffer ()
+  "Rename the current buffer and file it is visiting."
+  (interactive)
+  (let ((filename (buffer-file-name)))
+    (if (not (and filename (file-exists-p filename)))
+        (message "Buffer is not visiting a file!")
+      (let ((new-name (read-file-name "New name: " filename)))
+        (cond
+         ((vc-backend filename) (vc-rename-file filename new-name))
+         (t
+          (rename-file filename new-name t)
+          (set-visited-file-name new-name t t)))))))
