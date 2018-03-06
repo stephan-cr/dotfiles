5d54f7ee8ec582df9128959f433b16011279a2df
stephan <stephan.cr@gmx.de>
2018-03-06 21:53:14 +0100

    add package.el initialization

--- init.el
+++ init.el
@@ -3,6 +3,13 @@
 ;; Author: Stephan Creutz
 
 ;; functions
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (defun on-host (hostname)
   "get the current hostname on which this instance is running"
   (string-equal (car (split-string system-name "\\.")) hostname))
