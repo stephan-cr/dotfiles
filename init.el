47888e195386fa61296b3e91bd33705a0c8ab4f4
stephan <stephan.cr@gmx.de>
2021-01-16 10:16:07 +0100

    add org-present to init.el

--- init.el
+++ init.el
@@ -650,6 +650,7 @@ The function assumes that the user set the variables
                            meson-mode
                            modern-cpp-font-lock
                            monky
+                           org-present
                            paredit
                            pretty-lambdada
                            project-explorer
@@ -720,6 +721,21 @@ The function assumes that the user set the variables
     (org-redisplay-inline-images)))
 
 (add-hook 'org-babel-after-execute-hook #'fix-inline-images)
+
+(eval-after-load "org-present"
+  '(progn
+     (add-hook 'org-present-mode-hook
+               (lambda ()
+                 (org-present-big)
+                 (org-display-inline-images)
+                 (org-present-hide-cursor)
+                 (org-present-read-only)))
+     (add-hook 'org-present-mode-quit-hook
+               (lambda ()
+                 (org-present-small)
+                 (org-remove-inline-images)
+                 (org-present-show-cursor)
+                 (org-present-read-write)))))
 
 (when (>= emacs-major-version 25)
   (eval-after-load 'bytecomp
