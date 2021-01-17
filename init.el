802a10afe9111c5e36f7ddb8a93fe59fbe651e7c
stephan <stephan.cr@gmx.de>
2021-01-17 23:41:32 +0100

    replace eval-after-load with with-eval-after-load

--- init.el
+++ init.el
@@ -725,20 +725,19 @@ The function assumes that the user set the variables
 
 (add-hook 'org-babel-after-execute-hook #'fix-inline-images)
 
-(eval-after-load "org-present"
-  '(progn
-     (add-hook 'org-present-mode-hook
-               (lambda ()
-                 (org-present-big)
-                 (org-display-inline-images)
-                 (org-present-hide-cursor)
-                 (org-present-read-only)))
-     (add-hook 'org-present-mode-quit-hook
-               (lambda ()
-                 (org-present-small)
-                 (org-remove-inline-images)
-                 (org-present-show-cursor)
-                 (org-present-read-write)))))
+(with-eval-after-load 'org-present
+  (add-hook 'org-present-mode-hook
+            (lambda ()
+              (org-present-big)
+              (org-display-inline-images)
+              (org-present-hide-cursor)
+              (org-present-read-only)))
+  (add-hook 'org-present-mode-quit-hook
+            (lambda ()
+              (org-present-small)
+              (org-remove-inline-images)
+              (org-present-show-cursor)
+              (org-present-read-write))))
 
 (when (>= emacs-major-version 25)
   (eval-after-load 'bytecomp
