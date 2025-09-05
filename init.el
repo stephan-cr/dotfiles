abf4a23466f738294d76e9db85b802c7e9e6ecf1
stephan <stephan.cr@gmx.de>
2025-09-05 22:07:36 +0200

    [emacs] add Ocaml modes

--- init.el
+++ init.el
@@ -607,6 +607,7 @@ The function assumes that the user set the variables
                             slime
                             sway
                             toml-mode
+                            tuareg-mode
                             volatile-highlights
                             which-key
                             yaml-mode
@@ -871,3 +872,19 @@ The function assumes that the user set the variables
                               emms-player-mpg321-url
                               emms-player-mpg321-list
                               emms-player-mpd))))
+
+;; Ocaml + Merlin
+(let ((opam-share (ignore-errors (car (process-lines "opam" "var" "share")))))
+  (when (and opam-share (file-directory-p opam-share))
+    ;; Register Merlin
+    (add-to-list 'load-path (expand-file-name "emacs/site-lisp" opam-share))
+    (autoload 'merlin-mode "merlin" nil t nil)
+    ;; Automatically start it in OCaml buffers
+    (add-hook 'tuareg-mode-hook 'merlin-mode t)
+    (add-hook 'caml-mode-hook 'merlin-mode t)
+    ;; Use opam switch to lookup ocamlmerlin binary
+    (setq merlin-command 'opam)
+    ;; To easily change opam switches within a given Emacs session, you can
+    ;; install the minor mode https://github.com/ProofGeneral/opam-switch-mode
+    ;; and use one of its "OPSW" menus.
+    ))
